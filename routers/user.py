b12f3c9f7c31fd2de8547a5072b395e16edd4832
jung jinyoung <ya_1001@naver.com>
2026-06-30 16:02:37 +0900

    feat: 5강 - 회원가입, 비밀번호 해싱 처리

--- routers/user.py
+++ routers/user.py
@@ -4,12 +4,19 @@ from sqlalchemy import select
 from schema.request import UserSignUPRequest
 from database.db_connection import SessionFactory
 from models import User
+
+from auth.password import hash_password
+from schema.response import UserSignUpResponse
+
 router = APIRouter(tags=["User"])
 
 # 회원가입
+
+
 @router.post(
     path="/users/signup",
     status_code=status.HTTP_201_CREATED,
+    response_model=UserSignUpResponse
 )
 def signup_user_handler(body: UserSignUPRequest):
     # 이메일 중복 검사
@@ -22,3 +29,18 @@ def signup_user_handler(body: UserSignUPRequest):
                 status_code=status.HTTP_409_CONFLICT,
                 detail="이미 사용 중인 이메일입니다."
             )
+
+        # 비밀번호 해시 생성
+        hashed_password = hash_password(body.password)
+
+        # User 모델 생성 후 DB 저장
+        user = User(
+            email=str(body.email),
+            hashed_password=hashed_password
+        )
+        # DB 저장
+        session.add(user)
+        session.commit()
+        # 응답 반환
+        session.refresh(user)  # DB에 생성된 값 반영
+        return user
